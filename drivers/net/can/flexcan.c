5b579c784f9b337a4d7cff3eebe1f26390c2235d
Grommerin <Grommerin@gmail.com>
2014-10-14 12:45:22 +0300

    Set lowest timings

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -213,9 +213,9 @@ static struct can_bittiming_const flexcan_bittiming_const = {
 	.tseg1_max = 4,
 	.tseg2_min = 1,
 	.tseg2_max = 2,
-	.sjw_max = 1,
+	.sjw_max = 2,
 	.brp_min = 1,
-	.brp_max = 32,
+	.brp_max = 16,
 	.brp_inc = 1,
 	/* Very low timings */
 	// .name = DRV_NAME,
